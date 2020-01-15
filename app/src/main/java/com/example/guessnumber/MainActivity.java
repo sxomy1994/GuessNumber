02599e59e5dc6303fbbfe964eb995117d81f0222
sxomy1994 <milos1994@live.com>
2020-01-15 17:30:56 +0100

    MainActivity.java
    updated version

--- app/src/main/java/com/example/guessnumber/MainActivity.java
+++ app/src/main/java/com/example/guessnumber/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.guessnumber;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -31,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         Random r = new Random();
         LinearLayout layout = (LinearLayout)findViewById(R.id.layout);
         EditText poljeZaBroj = (EditText)findViewById(R.id.enter_number);
-        TextView rezultatPogadjanja = new TextView(this);
+        final TextView rezultatPogadjanja = new TextView(this);
 
         String sBroj =poljeZaBroj.getText().toString();
         if(sBroj.matches("")){
@@ -39,33 +40,45 @@ public class MainActivity extends AppCompatActivity {
         }else{
             int iBroj = Integer.parseInt(sBroj);
             if(iBroj > randomNumber){
-                pokusaji++;
-                displayResult("The number is lower" +"\nBroj pokusaja: " + pokusaji +"\n" +
-                        "Entered numbers: " );
-                TextView rez = (TextView)findViewById(R.id.rezultat);
-                rez.setTextColor(Color.parseColor("#FF0000"));
-                unesiBroj(iBroj);
-                ispisi();
+                if(pokusaji < 10) {
+                    pokusaji++;
+                    displayResult("The number is lower" + "\nBroj pokusaja: " + pokusaji + "\n" +
+                            "Entered numbers: ");
+                    TextView rez = (TextView) findViewById(R.id.rezultat);
+                    rez.setTextColor(Color.parseColor("#FF0000"));
+                    unesiBrojIIspisi(iBroj);
+                } else{
+                    final TextView rez = (TextView)findViewById(R.id.rezultat);
+                    rez.setText("Sorry, you lost. Try Again!");
+                    rez.setTextColor(Color.parseColor("#FF0000"));
+
+                   reset();
+                }
 
             } else if(iBroj < randomNumber){
+                if(pokusaji < 10){
                 pokusaji++;
                 displayResult("The number is higher" +"\nBroj pokusaja:" +  pokusaji+"\n" +
                         "Entered numbers: " );
                 TextView rez = (TextView)findViewById(R.id.rezultat);
                 rez.setTextColor(Color.parseColor("#FF0000"));
-                unesiBroj(iBroj);
-                ispisi();
+                unesiBrojIIspisi(iBroj);}
+                else{
+                    TextView rez = (TextView)findViewById(R.id.rezultat);
+                    rez.setText("Sorry, you lost. Try Again!");
+                    rez.setTextColor(Color.parseColor("#FF0000"));
+
+                    reset();
+                }
+
             } else{
                 pokusaji++;
                 displayResult("CORRECT!" + "\nBroj pokusaja: " + pokusaji +"\n" +
                         "Entered numbers: " );
-                unesiBroj(iBroj);
-                ispisi();
+                unesiBrojIIspisi(iBroj);
                 TextView rez = (TextView)findViewById(R.id.rezultat);
                 rez.setTextColor(Color.parseColor("#00FF00"));
-                Button b = (Button)findViewById(R.id.button_try);
-                b.setClickable(false);
-                b.setBackgroundColor(Color.parseColor("#C0C0C0"));
+               reset();
             }
         }
     }
@@ -74,15 +87,45 @@ public class MainActivity extends AppCompatActivity {
         rez.setText(s + "");
         rez.setVisibility(View.VISIBLE);
     }
-    public void unesiBroj(int br){
-        pokusajiNiz[brojac] = br;
-        brojac++;
-    }
-    public void ispisi(){
-        TextView tv = (TextView)findViewById(R.id.rezultat);
-        for(int i = 0; i  < brojac; i++){
-            tv.append(pokusajiNiz[i] + " ");
+    public void unesiBrojIIspisi(int br){
+        if(brojac < pokusajiNiz.length){
+            pokusajiNiz[brojac] = br;
+            brojac++;
+            TextView tv = (TextView)findViewById(R.id.rezultat);
+            for(int i = 0; i  < brojac; i++){
+                tv.append(pokusajiNiz[i] + " ");
+            }
+        }else{
+            TextView tv = (TextView)findViewById(R.id.rezultat);
+            tv.append("\nSorry, you didn't guess. Try Again!");
         }
+
     }
+    public void reset(){
+        final TextView rez = (TextView)findViewById(R.id.rezultat);
+        LinearLayout layout = (LinearLayout)findViewById(R.id.layout);
+        final Button b = (Button)findViewById(R.id.button_try);
+        b.setClickable(false);
+        b.setBackgroundColor(Color.parseColor("#C0C0C0"));
+        final Button reset = new Button(this);
+        reset.setText("Reset");
+        reset.setGravity(Gravity.CENTER);
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random r = new Random();
+                randomNumber = r.nextInt(20)+1;
+                pokusaji = 0;
+                brojac = 0;
+                rez.setText("");
+                b.setClickable(true);
+                b.setBackgroundColor(Color.parseColor("#7CFC00"));
+                reset.setVisibility(View.INVISIBLE);
+
+            }
+        });
+        layout.addView(reset);
+    }
+
 }
 
